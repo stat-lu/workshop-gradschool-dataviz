24f17091278d3c9f728137b8ac09fae76b534c93
Johan Larsson <johanlarsson@outlook.com>
2022-09-30 11:39:29 +0200

    feat: upload material for friday's workshop

--- data-raw/xy.R
+++ data-raw/xy.R
@@ -2,8 +2,8 @@ library(datasauRus)
 library(tidyverse)
 
 xy <-
-  datasaurus_dozen %>%
-  filter(dataset == "dino") %>%
+  datasaurus_dozen |>
+  filter(dataset == "dino") |>
   select(x, y)
 
 write_csv(xy, "data/xy.csv")
